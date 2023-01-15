7679a50f048f4c29ec1f41b3119fb1346b54954c
avivenzio <alex.vivenzio@gmail.com>
2023-01-15 16:49:42 -0500

    client build & copy script

---
+++ scripts/client_build_copy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Build & Copy client"
+cd ../client
+echo "Running npm install..."
+npm install
+echo "Building..."
+npm run build
+cd ..
+echo "Copying to /home/pi/boat-stuff-client"
+scp -r ./client/dist pi@192.168.50.216:/home/pi/boat-stuff-client
+scp -r ./client/package.json pi@192.168.50.216:/home/pi/boat-stuff-client
+echo "Run npm start:prod"
