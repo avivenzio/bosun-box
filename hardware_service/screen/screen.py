b044a5d75863490959c066d846fdc4a90bf984aa
avivenzio <alex.vivenzio@gmail.com>
2023-01-16 18:32:08 -0500

    add brightness controller

--- hardware_service/screen/screen.py
+++ hardware_service/screen/screen.py
@@ -1,9 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from screen.screen_io import set_brightness, get_brightness
 
 screen_bp = Blueprint('screen', __name__)
 
-@screen.route("/screen")
-def hello():
+@screen_bp.route("/screen", methods=['GET', 'PATCH'])
+def screen_resource():
+    headers = {"Content-Type": "application/json"}
+    if request.method == 'PATCH':
+        req_body = request.json
+        if req_body.get('brightness', None) is not None:
+            set_brightness(req_body["brightness"])
     return {
-        "status": "SCREEN"
+        "brightness": get_brightness()
     }
---
+++ hardware_service/screen/screen_io.py
@@ -0,0 +1,10 @@
+from gpiozero import PWMLED
+from time import sleep
+
+BRIGHTNESS_PIN = PWMLED(18)
+
+def set_brightness(value: float):
+    BRIGHTNESS_PIN.value = value
+
+def get_brightness():
+    return BRIGHTNESS_PIN
