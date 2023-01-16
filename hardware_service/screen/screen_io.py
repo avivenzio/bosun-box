9bf3c21a993343ed462800282154c00c61a85715
avivenzio <alex.vivenzio@gmail.com>
2023-01-16 18:39:27 -0500

    add log

--- hardware_service/screen/screen_io.py
+++ hardware_service/screen/screen_io.py
@@ -1,10 +1,13 @@
+import logging
 from gpiozero import PWMLED
 from time import sleep
 
 BRIGHTNESS_PIN = PWMLED(18)
 
 def set_brightness(value: float):
+    logging.info(f"set screen brightness value - {value}")
     BRIGHTNESS_PIN.value = value
 
 def get_brightness():
+    logging.info(f"get screen brightness value - {BRIGHTNESS_PIN.value}")
     return BRIGHTNESS_PIN.value
